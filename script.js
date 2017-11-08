a35b38f009e02ae9918e9813b12ea444a676fd54
psegenmark <psegenmark@gmail.com>
2017-11-08 11:50:15 -0800

    letsTryAgain

--- script.js
+++ script.js
@@ -60,26 +60,53 @@
     console.log(snapshot.val().trainTime1);
   
     var trs = $("<tr>");
+    
+    
     var timeNow = moment().format('HH:mm:ss');
+
+    console.log(timeNow);
+
    
     // Time for first train, minus current time. Take the result and divide by frequency. Round that number up to have next departure. That time minus current time is time left.
     
+  
+  // var tFrequency = (snapshot.val().trainTime2);
+  // console.log(tFrequency);
+  // var tTime = (snapshot.val().trainTime1);
+  // console.log(tTime);
 
+  // var firstTimeConverted = moment(tTime, "hh:mm").subtract(1, "years");
+  // console.log(firstTimeConverted);
+
+  // var currentTime = moment();
+  
+  // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  // console.log(diffTime);
+
+  // var tRemainder = diffTime % tFrequency;
+  // console.log(tRemainder);
+
+  // var tMinutesTillTrain = tFrequency - tRemainder;
+  // console.log(tMinutesTillTrain);
+
+  // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  // console.log("Arrival Time:" +moment(nextTrain).format("hh:mm"));
   
   var a = timeNow.split(':'); // split it at the colons
   var secondsNow = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);  
+  console.log(secondsNow);
   
   
-
+    // when is first train leaving?
   var b = snapshot.val().trainTime1.split(':')
 
   var secondsTrain = ((+b[0]) * 60 * 60 + (+b[1]) * 60);  
+  console.log(secondsTrain);  
 
+  var nextTrain = (secondsNow-secondsTrain) 
 
-  var nextTrain = Math.abs(secondsNow-secondsTrain) 
+  console.log(nextTrain);
 
-
-  
   // / ((snapshot.val().trainTime2) * 60)
 
   var minToNextTrain = (nextTrain/((snapshot.val().trainTime2)*60));
@@ -96,7 +123,8 @@
 
   var nextTrainHHMM = moment().startOf('day')
                               .seconds(trainF)
-                              .format('HH:mm:ss');
+                              .format('HH:mm');
+  console.log(nextTrainHHMM);
 
 
   td1.append(snapshot.val().trainName);
